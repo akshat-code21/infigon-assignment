667b330e5452a80cc502de749fdc16119f78e460
Akshat Sipany <borncancer21@gmail.com>
2025-03-29 23:25:21 +0530

    implemented footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import "./globals.css"
 import ReportHeader from "@/components/Header/ReportHeader"
 import CareerOptionsSection from "@/components/Career/CareerOptionsSelection"
 import InputDesign from "@/components/InputDesign"
+import Footer from "@/components/Footer/Footer"
 
 export default function Home() {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
       <div className="mt-10 px-4 md:px-6 lg:px-8">
         <ReportHeader />
@@ -15,6 +16,7 @@ export default function Home() {
       <div className="flex w-full">
         <InputDesign />
       </div>
-    </>
+      <Footer />
+    </div>
   )
 }
